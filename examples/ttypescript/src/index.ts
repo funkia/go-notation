75b07a7ef0c6d931bf53b673e907a4019ee9ab1e
Jonas Holst Damtoft <jonasdamtoft@hotmail.com>
2019-01-30 22:36:40 +0100

    find identifier and value to bind

--- examples/ttypescript/src/index.ts
+++ examples/ttypescript/src/index.ts
@@ -1,4 +1,4 @@
-import { go } from "../../../";
+import { go } from "go-notation";
 
 class Maybe<A> {
   constructor(private value: A) {}
@@ -7,27 +7,39 @@ class Maybe<A> {
   }
 }
 
-function test0() {
+function simple() {
   return go(bind => {
     const a = bind(new Maybe(12));
-    const b = bind(new Maybe("horse"));
-    const c = bind(new Maybe(2)) + bind(new Maybe(12));
-    return new Maybe(a.toString() + b + c);
+    return new Maybe(a * 2);
   });
 }
-
-function test1() {
-  return new Maybe(12).flatMap(a => {
-    return new Maybe("horse").flatMap(b => {
-      return new Maybe(2).flatMap(temp => {
-        return new Maybe(12).flatMap(temp2 => {
-          const c = temp + temp2;
-          return new Maybe(a.toString() + b + c);
-        });
-      });
-    });
-  });
+function simpleRes() {
+  return new Maybe(12).flatMap(a => new Maybe(a * 2));
 }
+simple();
+simpleRes();
 
-test0();
-test1();
+// function test0() {
+//   return go(bind => {
+//     const a = bind(new Maybe(12));
+//     const b = bind(new Maybe("horse"));
+//     const c = bind(new Maybe(2)) + bind(new Maybe(12));
+//     return new Maybe(a.toString() + b + c);
+//   });
+// }
+
+// function test1() {
+//   return new Maybe(12).flatMap(a => {
+//     return new Maybe("horse").flatMap(b => {
+//       return new Maybe(2).flatMap(temp => {
+//         return new Maybe(12).flatMap(temp2 => {
+//           const c = temp + temp2;
+//           return new Maybe(a.toString() + b + c);
+//         });
+//       });
+//     });
+//   });
+// }
+
+// test0();
+// test1();
--- src/transform.ts
+++ src/transform.ts
@@ -15,22 +15,84 @@ import * as ts from "typescript";
 //   };
 // }
 
-export default function(program: ts.Program, pluginOptions: {}) {
-  return (ctx: ts.TransformationContext) => {
-    return (sourceFile: ts.SourceFile) => {
-      function visitor(node: ts.Node): ts.Node {
-        if (ts.isVariableDeclaration(node)) {
-          // console.log(node);
-        }
-        if (
-          ts.isCallExpression(node) &&
-          node.expression.getText(sourceFile) === "go"
-        ) {
-          console.log(node);
-        }
-        return ts.visitEachChild(node, visitor, ctx);
-      }
-      return ts.visitEachChild(sourceFile, visitor, ctx);
-    };
-  };
+export default function transformer(
+  program: ts.Program
+): ts.TransformerFactory<ts.SourceFile> {
+  return (context: ts.TransformationContext) => (file: ts.SourceFile) =>
+    visitNodeAndChildren(file, program, context);
 }
+
+function visitNodeAndChildren(
+  node: ts.SourceFile,
+  program: ts.Program,
+  context: ts.TransformationContext
+): ts.SourceFile;
+function visitNodeAndChildren(
+  node: ts.Node,
+  program: ts.Program,
+  context: ts.TransformationContext
+): ts.Node;
+function visitNodeAndChildren(
+  node: ts.Node,
+  program: ts.Program,
+  context: ts.TransformationContext
+): ts.Node {
+  return ts.visitEachChild(
+    visitNode(node, program),
+    childNode => visitNodeAndChildren(childNode, program, context),
+    context
+  );
+}
+
+function visitNode(node: ts.Node, program: ts.Program): ts.Node {
+  if (ts.isCallExpression(node) && node.expression.getText() === "go") {
+    const arg0 = node.arguments[0];
+    if (ts.isArrowFunction(arg0)) {
+      const statements = arg0.body
+        .getChildAt(1)
+        .getChildren() as ts.Statement[];
+      for (const s of statements) {
+        if (ts.isVariableStatement(s)) {
+          const parts = s
+            .getChildren()[0]
+            .getChildren()[1]
+            .getChildren()[0];
+          if (ts.isVariableDeclaration(parts)) {
+            const exp = parts.getChildAt(2);
+            if (
+              ts.isCallExpression(exp) &&
+              exp.expression.getText() === "bind"
+            ) {
+              const identifier = ts.createIdentifier(
+                parts.getChildAt(0).getText()
+              );
+              const val = exp.arguments[0];
+              // Somehow do `val`.flatMap(`identifier` => rest of statements)
+            }
+          }
+        }
+      }
+    }
+  }
+  return node;
+}
+
+// export default function(program: ts.Program, pluginOptions: {}) {
+//   return (ctx: ts.TransformationContext) => {
+//     return (sourceFile: ts.SourceFile) => {
+//       function visitor(node: ts.Node): ts.Node {
+//         if (ts.isVariableDeclaration(node)) {
+//           // console.log(node);
+//         }
+//         if (
+//           ts.isCallExpression(node) &&
+//           node.expression.getText(sourceFile) === "go"
+//         ) {
+//           console.log(node);
+//         }
+//         return ts.visitEachChild(node, visitor, ctx);
+//       }
+//       return ts.visitEachChild(sourceFile, visitor, ctx);
+//     };
+//   };
+// }
