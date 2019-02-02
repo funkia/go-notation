391af88ca5d5c60ad6ebdb24deab4e5180b6cacd
Jonas Holst Damtoft <jonasdamtoft@hotmail.com>
2019-02-02 17:45:28 +0100

    nix: use nodejs_11

--- shell.nix
+++ shell.nix
@@ -1,9 +1,15 @@
-with import <nixpkgs> {};
+with import (builtins.fetchGit {
+  name = "nixpkgs-unstable-2019-02-02";
+  url = https://github.com/nixos/nixpkgs/;
+  rev = "11cf7d6e1ffd5fbc09a51b76d668ad0858a772ed";
+}) {};
+
+
 
 let
   inherit (pkgs) mkShell;
 in mkShell {
   buildInputs = with pkgs; [
-    nodejs-10_x
+    nodejs-11_x
   ];
 }
