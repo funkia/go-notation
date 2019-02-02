a83162366efb63745f95c2c68979ed28ae4fbd3b
paldepind <simonfv@gmail.com>
2019-02-02 18:47:52 +0100

    Add additional test

--- test/index.ts
+++ test/index.ts
@@ -4,10 +4,26 @@ import * as assert from "assert";
 import { go } from "../src/index";
 
 describe("go-notation", () => {
-  it("works in simple case", () => {
+  it("works in simple case with arrow function", () => {
     const array = go(bind => {
       const a = bind([1, 2]);
       const b = bind([3, 4]);
+      return [a, b];
+    });
+    assert.deepEqual(array, [1, 3, 1, 4, 2, 3, 2, 4]);
+  });
+  it("supports custom bind name", () => {
+    const array = go(foobar => {
+      const a = foobar([1, 2]);
+      const b = foobar([3, 4]);
+      return [a, b];
+    });
+    assert.deepEqual(array, [1, 3, 1, 4, 2, 3, 2, 4]);
+  });
+  it("handles anonymous functions without arrow", () => {
+    const array = go(function(foobar) {
+      const a = foobar([1, 2]);
+      const b = foobar([3, 4]);
       return [a, b];
     });
     assert.deepEqual(array, [1, 3, 1, 4, 2, 3, 2, 4]);
