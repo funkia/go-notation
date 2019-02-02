98cdfa8b17649f040c681c044ed4aa02256a0ada
Jonas Holst Damtoft <jonasdamtoft@hotmail.com>
2019-02-02 11:01:53 +0100

    Working dumb transformation

--- examples/ttypescript/src/index.ts
+++ examples/ttypescript/src/index.ts
@@ -10,6 +10,7 @@ class Maybe<A> {
 function simple() {
   return go(bind => {
     const a = bind(new Maybe(12));
+    const b = bind(new Maybe(42));
     return new Maybe(a * 2);
   });
 }
@@ -19,27 +20,29 @@ function simpleRes() {
 simple();
 simpleRes();
 
-// function test0() {
-//   return go(bind => {
-//     const a = bind(new Maybe(12));
-//     const b = bind(new Maybe("horse"));
-//     const c = bind(new Maybe(2)) + bind(new Maybe(12));
-//     return new Maybe(a.toString() + b + c);
-//   });
-// }
+function test0() {
+  return go(bind => {
+    const a = bind(new Maybe(12));
+    const b = bind(new Maybe("horse"));
+    const c1 = bind(new Maybe(2));
+    const c2 = bind(new Maybe(12));
+    const c = c1 + c2;
+    return new Maybe(a.toString() + b + c);
+  });
+}
 
-// function test1() {
-//   return new Maybe(12).flatMap(a => {
-//     return new Maybe("horse").flatMap(b => {
-//       return new Maybe(2).flatMap(temp => {
-//         return new Maybe(12).flatMap(temp2 => {
-//           const c = temp + temp2;
-//           return new Maybe(a.toString() + b + c);
-//         });
-//       });
-//     });
-//   });
-// }
+function test1() {
+  return new Maybe(12).flatMap(a => {
+    return new Maybe("horse").flatMap(b => {
+      return new Maybe(2).flatMap(temp => {
+        return new Maybe(12).flatMap(temp2 => {
+          const c = temp + temp2;
+          return new Maybe(a.toString() + b + c);
+        });
+      });
+    });
+  });
+}
 
 // test0();
 // test1();
--- src/transform.ts
+++ src/transform.ts
@@ -1,47 +1,40 @@
 import * as ts from "typescript";
 
-// export default function(/*opts?: Opts*/) {
-//   function visitor(ctx: ts.TransformationContext, sf: ts.SourceFile) {
-//     const visitor: ts.Visitor = (node: ts.Node): ts.VisitResult => {
-//       // here we can check each node and potentially return
-//       // new nodes if we want to leave the node as is, and
-//       // continue searching through child nodes:
-//       return ts.visitEachChild(node, visitor, ctx);
-//     };
-//     return visitor;
-//   }
-//   return (ctx: ts.TransformationContext): ts.Transformer => {
-//     return (sf: ts.SourceFile) => ts.visitNode(sf, visitor(ctx, sf));
-//   };
-// }
+export default function transformer(): ts.TransformerFactory<ts.SourceFile> {
+  return (context: ts.TransformationContext) => {
+    return (file: ts.SourceFile) => ts.visitEachChild(file, visitor, context);
 
-export default function transformer(
-  program: ts.Program
-): ts.TransformerFactory<ts.SourceFile> {
-  return (context: ts.TransformationContext) => (file: ts.SourceFile) =>
-    visitNodeAndChildren(file, program, context);
+    function visitor(node: ts.Node): ts.VisitResult<ts.Node> {
+      if (ts.isCallExpression(node) && node.expression.getText() === "go") {
+        return visitGoBody(<ts.CallExpression>node);
+      }
+      return ts.visitEachChild(node, visitor, context);
+    }
+  };
 }
 
-function visitNodeAndChildren(
-  node: ts.SourceFile,
-  program: ts.Program,
-  context: ts.TransformationContext
-): ts.SourceFile;
-function visitNodeAndChildren(
-  node: ts.Node,
-  program: ts.Program,
-  context: ts.TransformationContext
-): ts.Node;
-function visitNodeAndChildren(
-  node: ts.Node,
-  program: ts.Program,
-  context: ts.TransformationContext
-): ts.Node {
-  return ts.visitEachChild(
-    visitNode(node, program),
-    childNode => visitNodeAndChildren(childNode, program, context),
-    context
-  );
+function visitGoBody(node: ts.CallExpression) {
+  const arg0 = node.arguments[0];
+  if (ts.isArrowFunction(arg0)) {
+    const statements = arg0
+      .getChildAt(2)
+      .getChildren()[1]
+      .getChildren() as ts.Statement[];
+
+    return ts.createCall(
+      ts.createFunctionExpression(
+        undefined,
+        undefined,
+        undefined,
+        undefined,
+        [],
+        undefined,
+        ts.createBlock(visitBinds(statements))
+      ),
+      undefined,
+      undefined
+    );
+  }
 }
 
 function visitBinds(statements: ts.Statement[]): ts.Statement[] {
@@ -93,49 +86,3 @@ function visitBinds(statements: ts.Statement[]): ts.Statement[] {
     return statements;
   }
 }
-
-function visitNode(node: ts.Node, program: ts.Program): ts.Node {
-  if (ts.isCallExpression(node) && node.expression.getText() === "go") {
-    const arg0 = node.arguments[0];
-    if (ts.isArrowFunction(arg0)) {
-      const statements = arg0
-        .getChildAt(2)
-        .getChildren()[1]
-        .getChildren() as ts.Statement[];
-
-      return ts.createCall(
-        ts.createArrowFunction(
-          undefined,
-          undefined,
-          [],
-          undefined,
-          undefined,
-          ts.createBlock(visitBinds(statements))
-        ),
-        undefined,
-        undefined
-      );
-    }
-  }
-  return node;
-}
-
-// export default function(program: ts.Program, pluginOptions: {}) {
-//   return (ctx: ts.TransformationContext) => {
-//     return (sourceFile: ts.SourceFile) => {
-//       function visitor(node: ts.Node): ts.Node {
-//         if (ts.isVariableDeclaration(node)) {
-//           // console.log(node);
-//         }
-//         if (
-//           ts.isCallExpression(node) &&
-//           node.expression.getText(sourceFile) === "go"
-//         ) {
-//           console.log(node);
-//         }
-//         return ts.visitEachChild(node, visitor, ctx);
-//       }
-//       return ts.visitEachChild(sourceFile, visitor, ctx);
-//     };
-//   };
-// }
